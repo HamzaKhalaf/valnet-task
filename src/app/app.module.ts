eb2d7472da4768845ead7a899abfbb03adcecfa5
hamza khalaf <hamza@realblocks.com>
2022-10-02 08:13:57 +0300

    projects manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProjectsTreeComponent } from './projects-tree/projects-tree.component';
+import { ProjectTaskComponent } from './projects-tree/project-task/project-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsTreeComponent,
+    ProjectTaskComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
